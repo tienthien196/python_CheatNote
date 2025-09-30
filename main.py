55ee8410cf2bf89e99c1c1041fd59b6590010a81
GM_Finn <votienthien.196@gmail.com>
2025-09-30 11:23:59 +0700

    up base

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-s = "Hello, World!"
-print(f"Uppercase: {s.upper()}")
+class people():
+
+    height  = 190
+    width = 80
+    def  __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def cancought(self):
+        print(f"hello {self}")
+
+
+
+class tienthien(people):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+if __name__ == "__main__":
+    p1 = people("Nguyen Van A", 20)
+    p2 = people("Nguyen Van B", 30)
+    print(p1.cancought)
+

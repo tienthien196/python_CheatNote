60db0947e7a772bbcf0ce1c49c49dadb02e853d5
GM_Finn <votienthien.196@gmail.com>
2025-11-16 23:47:52 +0700

    _

--- 003_SUPPOPRT/tools/click.py
+++
@@ -1,9 +0,0 @@
-import click
-
-@click.command()
-@click.option('--name', prompt='Your name', help='The person to greet.')
-def hello(name):
-    click.echo(f'Hello, {name}!')
-
-if __name__ == '__main__':
-    hello()
--- Prototype/test3.py
+++
@@ -1,3 +0,0 @@
-from termcolor import colored
-
-print(colored('Đây là văn bản màu đỏ', 'red'))
